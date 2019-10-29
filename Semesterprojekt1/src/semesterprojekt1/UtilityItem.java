705c9886e6e4463f182740446da36d2ad2e3bdcc
Markus Munk <markusmunks@gmail.com>
2019-10-29 13:53:31 +0100

    Item branch - done

--- Semesterprojekt1/src/semesterprojekt1/UtilityItem.java
+++ Semesterprojekt1/src/semesterprojekt1/UtilityItem.java
@@ -9,6 +9,28 @@ package semesterprojekt1;
  *
  * @author aalbaek-nt
  */
-public class UtilityItem {
+public class UtilityItem extends Item {
+    
+    private int points;
+    
+    /**
+     * 
+     * @param name name for item
+     * @param description description for the item
+     * @param itemSize the amount of space item take in inventory.
+     * @param points the points the utilityItem can give from treatment. 
+     */
+    public UtilityItem(String name, String description, int itemSize, int points){
+        super(name, description, itemSize);
+        this.points = points;
+    }
+    
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
     
 }
