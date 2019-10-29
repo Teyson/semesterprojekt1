27c41dbbc826041a89556acd43690203e278171e
Markus Munk <markusmunks@gmail.com>
2019-10-29 14:57:41 +0100

    Room umildbar færdig,
    Der er meldt en fejl, fordi der ikke er et argument på inventory constructor

--- Semesterprojekt1/src/semesterprojekt1/Room.java
+++ Semesterprojekt1/src/semesterprojekt1/Room.java
@@ -1,49 +1,77 @@
 package semesterprojekt1;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.Iterator;
 
+public class Room {
 
-public class Room 
-{
     private String description;
     private HashMap<String, Room> exits;
+    private HashMap<String, NPC> NPCs;
+    private Inventory items;
+    private Inventory truckInventory;
 
-    public Room(String description) 
-    {
+    public Room(String description, int truckSize) {
         this.description = description;
         exits = new HashMap<String, Room>();
+        this.items = new Inventory();
+        this.truckInventory = new Inventory(truckSize);
+        this.NPCs = new HashMap<String, NPC>();
     }
 
-    public void setExit(String direction, Room neighbor) 
-    {
+    public Room(String description) {
+        this.description = description;
+        exits = new HashMap<String, Room>();
+        this.items = new Inventory();
+        this.NPCs = new HashMap<String, NPC>();
+    }
+
+    public void setNPC(NPC npc) {
+        if (npc instanceof NPC) {
+            this.NPCs.put(npc.getName(), npc);
+        } else {
+            System.out.println("Not an NPC");
+        }
+
+    }
+
+    public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
 
-    public String getShortDescription()
-    {
+    public String getShortDescription() {
         return description;
     }
 
-    public String getLongDescription()
-    {
+    public String getLongDescription() {
         return "You are " + description + ".\n" + getExitString();
     }
 
-    private String getExitString()
-    {
+    private String getExitString() {
         String returnString = "Exits:";
         Set<String> keys = exits.keySet();
-        for(String exit : keys) {
+        for (String exit : keys) {
             returnString += " " + exit;
         }
         return returnString;
     }
 
-    public Room getExit(String direction) 
-    {
+    public Room getExit(String direction) {
         return exits.get(direction);
     }
-}
 
+    public HashMap<String, NPC> getNPCs() {
+        return NPCs;
+    }
+
+    public Inventory getItems() {
+        return items;
+    }
+
+    public Inventory getTruckInventory() {
+        return truckInventory;
+    }
+
+}
