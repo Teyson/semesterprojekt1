67d028cfdbdc0682fd240dd77b234eeec0d24278
Sarah Manon Pradel <sapra18@student.sdu.dk>
2019-11-12 10:50:09 +0100

    Added condom to testroom and fixed some words :)

--- Semesterprojekt1/src/semesterprojekt1/Game.java
+++ Semesterprojekt1/src/semesterprojekt1/Game.java
@@ -9,10 +9,11 @@ public class Game {
     private String playerName;
 
     private Room spawn, info, testRoom;
-    private Interaction MariaI, MariaI2, MariaInfoI, MariaInfoI2, MariaInfoI3, HandbookI, HandbookHIV, HandbookTUR, HandbookMAL, PatientI, PatientI2, PatientI3, PatientI4;
+    private Interaction MariaI, MariaI2, MariaInfoI, MariaInfoI2, MariaInfoI3, HandbookI, HandbookHIV, HandbookTUB, HandbookMAL, PatientI, PatientI2, PatientI3, PatientI4;
     private NPC Maria, MariaInfo, Patient;
     private UtilityItem Handbook;
     private MedicineItem medicine;
+    private UtilityItem condom;
     private boolean interactedMaria = false; //boolean for checking if you have interacted with Maria in spawn
 
     public Game() {
@@ -28,7 +29,7 @@ public class Game {
         // rooms
         spawn = new Room("in the 'WHO' training facilities in Genéva");
         info = new Room("in the information center");
-        testRoom = new Room("in a test facility. Here you will try to cure a patient");
+        testRoom = new Room("in the test facility. Here you will try to cure a patient");
 
         // exists
         spawn.setExit("west", info);
@@ -41,12 +42,12 @@ public class Game {
 
     private void createInteraction() {
         //Spawn
-        MariaI = new Interaction("root", "Hello my name is Maria, thank you so much for volunteering! I'll be your guide.");
-        MariaI2 = new Interaction("What do I do now?", "Go to the information center to the west to learn more about your role in this operation.");
+        MariaI = new Interaction("root", "Hello my name is Maria, and thank you so much for volunteering! I'll be your guide.");
+        MariaI2 = new Interaction("What do I do now?", "Please go to the information center to the west, to learn more about your role in this operation.");
         MariaI.addChild(MariaI2);
 
         //Information center
-        MariaInfoI = new Interaction("root", "You're on your way to Mozambique, it is therefore important you have some knowledge about the diseases you'll encounter.");
+        MariaInfoI = new Interaction("root", "You're on your way to Mozambique, it is therefore important you have some knowledge about the diseases you'll most likely be treating.");
         MariaInfoI2 = new Interaction("Which diseases will I encounter?", "You will encounter HIV, Tuberculosis and Malaria");
         MariaInfoI3 = new Interaction("I think I'm ready. Can I go now?", "No! Not before you have the Doctor's Handbook, grab one from the shelf before you leave");
         MariaInfoI.addChild(MariaInfoI2);
@@ -55,16 +56,16 @@ public class Game {
 
         //Handbook
         HandbookI = new Interaction("root", "Table of contents:");
-        HandbookHIV = new Interaction("HIV", "People with HIV usually have these symptoms: headache and fatigue.");
-        HandbookTUR = new Interaction("Tuberculosis", "People with Turberculosis usually have these symptoms: coughing and chest pain.");
-        HandbookMAL = new Interaction("Malaria", "People with Malaria usually have these symptoms: fever and nausea.");
+        HandbookHIV = new Interaction("HIV", "People with HIV usually have the following symptoms: headache and fatigue.");
+        HandbookTUB = new Interaction("Tuberculosis", "People with Tuberculosis usually have the following symptoms: coughing and chest pain.");
+        HandbookMAL = new Interaction("Malaria", "People with Malaria usually have the following symptoms: fever and nausea.");
         HandbookI.addChild(HandbookHIV);
-        HandbookI.addChild(HandbookTUR);
+        HandbookI.addChild(HandbookTUB);
         HandbookI.addChild(HandbookMAL);
 
         //TestRoom
         PatientI = new Interaction("root", "Please help me! My body is very warm and my skin is red");
-        PatientI2 = new Interaction("Look for more Symptoms", "You see that the patient's throat is red, she has a high fever and a red rash all over her body");
+        PatientI2 = new Interaction("Look for more symptoms", "You see that the patient's throat is red, she has a high fever and a red rash all over her body");
         PatientI3 = new Interaction("Treat", "What would you like to treat the patient with?");
         PatientI4 = new Interaction("Give item", "Which item would you like to give the patient?");
         PatientI.addChild(PatientI2);
@@ -90,12 +91,15 @@ public class Game {
 
     private void createItem() {
         //Information center
-        Handbook = new UtilityItem("Handbook", "You can use this book to gain knowledge about diseases, to help you give the correct diagnosis.", 0, 0, HandbookI);
+        Handbook = new UtilityItem("Handbook", "A Doctor's Handbook giving fundamental knowledge about infectious diseases and their symptoms", 0, 0, HandbookI);
         info.addItem(Handbook.getName(), Handbook);
 
         //TestRoom
-        medicine = new MedicineItem("Medicine", "This will treat the patient.", 1, "HIV");
+        medicine = new MedicineItem("Medicine", "This will treat the test Patient.", 1, "HIV");
         testRoom.addItem(medicine.getName(), medicine);
+        
+        condom = new UtilityItem("Condom", "This will protect you and your sexual partner from Sexually Transmitted Infections (STIs)", 1, 5);
+        testRoom.addItem(condom.getName(), condom);
     }
 
     public void play() {
@@ -112,7 +116,7 @@ public class Game {
     private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to Kill the Killers!");
-        System.out.println("Where your job is to stop the spread of some of the biggest infectious diseases.");
+        System.out.println("Where your job is to stop the spread of some of the biggest infectious diseases in vulnerable countries!");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
@@ -141,7 +145,7 @@ public class Game {
                                 System.out.println("'interact' - Starts a conversation with a NPC.");
                                 break OUTER;
                             case "take":
-                                System.out.println("'take' - Takes an item and stores it in you inventory.");
+                                System.out.println("'take' - Takes an item and stores it in your inventory.");
                                 break OUTER;
                             case "open":
                                 System.out.println("'open' - Opens the Handbook & the Inventory.");
@@ -177,7 +181,7 @@ public class Game {
                     if (command.getSecondWord() == null) {
                         System.out.println(currentRoom.getLongDescription());
                     } else {
-                        System.out.println("You can only type info by itself");
+                        System.out.println("The info command has no second words.");
                     }
                 default:
                     break;
@@ -227,7 +231,7 @@ public class Game {
 
                 tempNPC.interact(playerInventory);
             } else {
-                System.out.println("Interact what?");
+                System.out.println("Interact with who?");
             }
         } catch (NullPointerException e) {
             System.out.println("No NPC with that name here.");
