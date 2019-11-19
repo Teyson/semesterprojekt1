4ad50591bc53517418da085da28093a41edb5484
Markus Munk <mmunk19@student.sdu.dk>
2019-11-19 15:19:20 +0100

    GUI default template
    dab on dem haters tHeis

--- GUISemesterProjekt1/src/main/java/module-info.java
+++ GUISemesterProjekt1/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module sp1.guisemesterprojekt1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens sp1.guisemesterprojekt1 to javafx.fxml;
     exports sp1.guisemesterprojekt1;
--- GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/App.java
+++ GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 800, 600);
+        scene = new Scene(loadFXML("startMenu"), 800, 600);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
