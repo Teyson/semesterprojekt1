9410d832fa84149631ee70c66250211edf461399
Markus Munk <mmunk19@student.sdu.dk>
2019-11-19 11:23:20 +0100

    default fxml fil
    dab

---
+++ GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp1.guisemesterprojekt1;
+
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author marku
+ */
+public class Main {
+    public static void main(String[] args) {
+        App.load(args);
+    }
+    
+}
---
+++ GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/PrimaryController.java
@@ -0,0 +1,12 @@
+package sp1.guisemesterprojekt1;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+  @FXML
+    private void switchToSecondary() throws IOException {
+       // App.setRoot("secondary");
+   }
+}
