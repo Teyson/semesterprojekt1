2dfec1a7374a38b192bb80db1b74866c6c215777
Sarah Manon Pradel <sapra18@student.sdu.dk>
2019-11-28 13:05:33 +0100

    Added Image views to inventory and game grid

--- GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/PrimaryController.java
+++ GUISemesterProjekt1/src/main/java/sp1/guisemesterprojekt1/PrimaryController.java
@@ -8,7 +8,6 @@ package sp1.guisemesterprojekt1;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -34,69 +33,139 @@ import javafx.stage.Stage;
 public class PrimaryController implements Initializable {
 
     //Setting the Time Indicator
-    @FXML
-    ProgressBar timeProgressBar;
+    @FXML ProgressBar timeProgressBar;
 
     //Setting the GridPane
-    @FXML
-    GridPane inventoryGrid;
-    @FXML
-    GridPane gameGrid;
-    @FXML
-    GridPane dialogOptionsGrid;
+    @FXML GridPane inventoryGrid;
+    @FXML GridPane gameGrid;
+    @FXML GridPane dialogOptionsGrid;
 
     //Setting the AnchorPane
-    @FXML
-    AnchorPane dialogPane;
-    @FXML
-    AnchorPane helpPopup;
+    @FXML AnchorPane dialogPane;
+    @FXML AnchorPane helpPopup;
 
     //Setting the sprites in the gridpane
-    @FXML
-    Circle testNPC;
+    @FXML Circle testNPC;
 
     //Setting the labels and their texts
-    @FXML
-    Label helpLabel;
-    @FXML
-    Label dialogLabel;
-    @FXML
-    Label NPCNameLabel;
+    @FXML Label helpLabel;
+    @FXML Label dialogLabel;
+    @FXML Label NPCNameLabel;
 
     //Setting the buttons
-    @FXML
-    Button treatBtn;
-    @FXML
-    Button giveItemBtn;
+    @FXML Button treatBtn;
+    @FXML Button giveItemBtn;
 
-    @FXML
-    Button answer1;
-    @FXML
-    Button answer2;
-    @FXML
-    Button answer3;
-    @FXML
-    Button answer4;
-    @FXML
-    Button answer5;
+    @FXML Button answer1;
+    @FXML Button answer2;
+    @FXML Button answer3;
+    @FXML Button answer4;
+    @FXML Button answer5;
 
     //Setting the ImageViews
-    @FXML
-    ImageView backgroundImage;
-    @FXML
-    ImageView NPCImage;
-    @FXML
-    ImageView field7_3;
-    @FXML
-    ImageView helpBtn;
-    @FXML
-    ImageView trashBtn;
-    @FXML
-    ImageView closeDialog;
-    @FXML
-    ImageView closeHelp;
-    @FXML
-    ImageView openHandbook;
+    @FXML ImageView backgroundImage;
+    @FXML ImageView NPCImage;
+    @FXML ImageView helpBtn;
+    @FXML ImageView trashBtn;
+    @FXML ImageView closeDialog;
+    @FXML ImageView closeHelp;
+    @FXML ImageView openHandbook;
+    
+    //Setting the Grid ImageViews
+    @FXML ImageView field0_0;
+    @FXML ImageView field0_1;
+    @FXML ImageView field0_2;
+    @FXML ImageView field0_3;
+    @FXML ImageView field0_4;
+    @FXML ImageView field0_5;
+    @FXML ImageView field0_6;
+    @FXML ImageView field1_0;
+    @FXML ImageView field1_1;
+    @FXML ImageView field1_2;
+    @FXML ImageView field1_3;
+    @FXML ImageView field1_4;
+    @FXML ImageView field1_5;
+    @FXML ImageView field1_6;
+    @FXML ImageView field2_0;
+    @FXML ImageView field2_1;
+    @FXML ImageView field2_2;
+    @FXML ImageView field2_3;
+    @FXML ImageView field2_4;
+    @FXML ImageView field2_5;
+    @FXML ImageView field2_6;
+    @FXML ImageView field3_0;
+    @FXML ImageView field3_1;
+    @FXML ImageView field3_2;
+    @FXML ImageView field3_3;
+    @FXML ImageView field3_4;
+    @FXML ImageView field3_5;
+    @FXML ImageView field3_6;
+    @FXML ImageView field4_0;
+    @FXML ImageView field4_1;
+    @FXML ImageView field4_2;
+    @FXML ImageView field4_3;
+    @FXML ImageView field4_4;
+    @FXML ImageView field4_5;
+    @FXML ImageView field4_6;
+    @FXML ImageView field5_0;
+    @FXML ImageView field5_1;
+    @FXML ImageView field5_2;
+    @FXML ImageView field5_3;
+    @FXML ImageView field5_4;
+    @FXML ImageView field5_5;
+    @FXML ImageView field5_6;
+    @FXML ImageView field6_0;
+    @FXML ImageView field6_1;
+    @FXML ImageView field6_2;
+    @FXML ImageView field6_3;
+    @FXML ImageView field6_4;
+    @FXML ImageView field6_5;
+    @FXML ImageView field6_6;
+    @FXML ImageView field7_0;
+    @FXML ImageView field7_1;
+    @FXML ImageView field7_2;
+    @FXML ImageView field7_3;
+    @FXML ImageView field7_4;
+    @FXML ImageView field7_5;
+    @FXML ImageView field7_6;
+    @FXML ImageView field8_0;
+    @FXML ImageView field8_1;
+    @FXML ImageView field8_2;
+    @FXML ImageView field8_3;
+    @FXML ImageView field8_4;
+    @FXML ImageView field8_5;
+    @FXML ImageView field8_6;
+    @FXML ImageView field9_0;
+    @FXML ImageView field9_1;
+    @FXML ImageView field9_2;
+    @FXML ImageView field9_3;
+    @FXML ImageView field9_4;
+    @FXML ImageView field9_5;
+    @FXML ImageView field9_6;
+    @FXML ImageView field10_0;
+    @FXML ImageView field10_1;
+    @FXML ImageView field10_2;
+    @FXML ImageView field10_3;
+    @FXML ImageView field10_4;
+    @FXML ImageView field10_5;
+    @FXML ImageView field10_6;
+    @FXML ImageView field11_0;
+    @FXML ImageView field11_1;
+    @FXML ImageView field11_2;
+    @FXML ImageView field11_3;
+    @FXML ImageView field11_4;
+    @FXML ImageView field11_5;
+    @FXML ImageView field11_6;
+    
+    //Setting the Inventory images
+    @FXML ImageView inv1;
+    @FXML ImageView inv2;
+    @FXML ImageView inv3;
+    @FXML ImageView inv4;
+    @FXML ImageView inv5;
+    @FXML ImageView inv6;
+    @FXML ImageView inv7;
+    @FXML ImageView inv8;
 
     //Initialising the image paths and setting them to an image
     //Buttons
